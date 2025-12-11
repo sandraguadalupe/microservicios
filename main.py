57594350f2e0e7eedb04a15c466f6eb12203b689
sandraguadalupe <hernandeznieto33@gmail.com>
2025-12-11 01:50:35 -0600

    Remove duplicate code for running the app

--- main.py
+++ main.py
@@ -84,3 +84,11 @@ def predict_house(data: dict):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
+import uvicorn
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
